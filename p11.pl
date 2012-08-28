d4f7df8503fabc1fe422a224920520e601380b87
JinBlack <jinblack17@gmail.com>
2012-08-28 15:49:58 +0200

    p11 Solved

---
+++ p11.pl
@@ -0,0 +1,10 @@
+encode([],[]).
+encode([X|Xs],[Z|Zs]) :- subpack(X,Xs,Ys,K),numbaPack(K,Z), encode(Ys,Zs).
+
+subpack(X,[],[],[X]).
+subpack(X,[Y|Ys],[Y|Ys],[X]) :- X \=Y.
+subpack(X,[X|Xs],Qs,[X|Zs]) :- subpack(X,Xs,Qs,Zs). 
+
+numbaPack([Z],Z).
+numbaPack([Z,Z],[K,Z]):- K is 2.
+numbaPack([Z|Zs],[N,Z]) :- numbaPack(Zs,[K,Z]),N is 1+K.
